e3a86d41a69693f9eae36a11c00a04552d8cfd3b
juphin <juphinmbaya0@gmail>
2022-04-23 01:34:02 +0100

    second commit

---
+++ main/main/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from rest_framework import routers
+from django.urls import path, include
+from api import views
+
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+# Setup automatic URL routing
+# Additionally, we include login URLs for the browsable API.
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
